77252c927ac60e2768e9bebfda765be0f33074dd
Artur0HM90 <arturohurtadomendez90@gmail.com>
2023-12-08 18:12:54 -0500

    renombrando el archivo

---
+++ 00_hello.py
@@ -0,0 +1,16 @@
+# Hola mundo
+print ("Hola Python")
+print ('Hola Python')
+
+
+# consutar el tipo de dato (type)
+print(type("Hola Python")) # tipo 'str'
+print(type(165145))        # tipo 'int'
+print(type(165145.546))    # tipo 'float'
+print(type(1 + 1j))        # tipo 'complex'
+
+"""
+
+este es un comentario en varias lineas
+
+"""
