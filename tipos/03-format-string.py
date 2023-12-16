5f65552b6b2288da576e8f3a52a68f43b421471c
Artur0HM90 <arturohurtadomendez90@gmail.com>
2023-12-16 17:11:47 -0500

    format-string

---
+++ tipos/03-format-string.py
@@ -0,0 +1,8 @@
+nombre = "Arturo"
+apellidos = "Hurtado Méndez"
+
+nombre_completo1 = nombre + " " + apellidos
+nombre_completo2 = f"{nombre} {apellidos}"
+nombre_completo3 = f"{nombre[0]} {2+5}"
+
+print(nombre_completo1, nombre_completo2, nombre_completo3)
