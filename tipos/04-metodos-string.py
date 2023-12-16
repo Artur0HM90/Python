360b59bb2f61d1503cccfb2fe419cc561bbe1fc4
Artur0HM90 <arturohurtadomendez90@gmail.com>
2023-12-16 18:02:35 -0500

    Metodos

---
+++ tipos/04-metodos-string.py
@@ -0,0 +1,27 @@
+animal = "  chanCHito feliZ   "
+print(animal.upper())
+print(animal.lower())
+print(animal.capitalize())
+print(animal.title())
+print(animal.strip())
+print(animal.strip().capitalize())
+print(animal.lstrip())
+print(animal.rstrip())
+print(animal.find("CH"))
+print(animal.replace("nCH", "j"))
+print("nCH" in animal)
+print("nCH" not in animal)
+
+
+# El upper() pondra todas las letras en MAYUSCULAS.
+# El lower() pondra todas las letras en minusculas.
+# El capitalize() pone la primra letra en MAYUSCULA y las demas en minusculas.
+# El title() pondra las primeras letas de todas la palabras en MAYUSCULAS.
+# El strip() quitara todos los espacios en blando que hay a la derecha o izquierda.
+# El stri().capitalize() el primero quita espacios y el segundo pone la primera letra en MAYUSULA.
+# El lstrip() quita los espacios de la izquierda.
+# El rstrip() quita los espaciosnde la derecha.
+# El find("ingresar el string a buscar") nos regresara el nivel del indice.
+# El replace("Ingresar los caracteres que se van a cambiar") nos sirve para cambiar caracteres.
+# El ("nCH" in animal) me dara un boolean me dira si es que se encuentra si o no.
+# El ("nCH" not in animal) me dara un boolean estoy preguntando si es que no se encuentra.
