4cc70c0123903a742fd797a3b28a51f93033aec4
Artur0HM90 <arturohurtadomendez90@gmail.com>
2023-12-16 21:06:25 -0500

    numeros

---
+++ tipos/06-numeros.py
@@ -0,0 +1,19 @@
+numero = 2              # entero integer
+decimal = 1.2           # float
+imaginario = 2 + 2j     # 2 + 2i
+
+# numero = numero + 2
+numero += 2
+numero -= 5
+numero *= 5
+numero /= 5
+
+print("numero", numero)
+
+print(1 + 3)
+print(1 - 3)
+print(1 * 3)
+print(1 / 3)
+print(1 // 3)
+print(8 % 3)  # cuando se utiliza en simbolo de porcentaje nos dice que de la division que hagas tienes que regresar el RESTO
+print(2 ** 3)  # El simbolo de ** es para elevar un número
