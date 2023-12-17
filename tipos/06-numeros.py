bae5da946cd21bbd3e4eeea02df671f31c4d0b85
Artur0HM90 <arturohurtadomendez90@gmail.com>
2023-12-16 21:10:08 -0500

    numeros

--- tipos/06-numeros.py
+++ tipos/06-numeros.py
@@ -15,5 +15,6 @@ print(1 - 3)
 print(1 * 3)
 print(1 / 3)
 print(1 // 3)
-print(8 % 3)  # cuando se utiliza en simbolo de porcentaje nos dice que de la division que hagas tienes que regresar el RESTO
+# cuando se utiliza en simbolo de porcentaje nos dice tienes que regresar el RESTO
+print(8 % 3)
 print(2 ** 3)  # El simbolo de ** es para elevar un número
