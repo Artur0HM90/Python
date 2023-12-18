6e5627e7f77a1b30e5c9f5491b1a8da657f320c5
Artur0HM90 <arturohurtadomendez90@gmail.com>
2023-12-18 13:30:33 -0500

    if ternarios

---
+++ control-flujo/03-if-ternario.py
@@ -0,0 +1,15 @@
+edad = 19
+
+#if edad > 17:
+#    print("Es mayor.")
+#else:
+#    print("Es menor.")
+# otra forma seria
+#if edad > 17:
+#    mensaje = "Es mayor."
+#else:
+#    mensaje = "Es menor."
+#print(mensaje)
+
+mensaje = "Es mayor" if edad > 17 else "Es menor"
+print(mensaje)
---
+++ control-flujo/04-operadores-logicos.py
@@ -0,0 +1,26 @@
+# and - or - not
+
+print("Operador logico AND los dos tienen que se verdaderos")
+gas = True
+encendido = True
+
+if gas and encendido:
+    print("puedes avanzar.")
+
+
+print("Operador logico OR solo que uno de ellos debe cumplirse")
+
+casa = False
+carro = True
+
+if casa or carro:
+    print("Puedes descansar")
+
+
+print("Operador logico NOT cambia el valor si es falso lo va a cambiar a verdadero")
+
+gas = False
+encendido = True
+
+if not gas and encendido:
+    print("puedes avanzar.")
