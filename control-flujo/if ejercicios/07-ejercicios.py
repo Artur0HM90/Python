433ed30793cd843803180515ea641bc8675f85a7
Artur0HM90 <arturohurtadomendez90@gmail.com>
2023-12-18 16:01:05 -0500

    modificando cadena de comparadores

--- control-flujo/if ejercicios/07-ejercicios.py
+++ control-flujo/if ejercicios/07-ejercicios.py
@@ -6,16 +6,30 @@ adolescente (13-19 años),
 adulto (20-64 años) o 
 adulto mayor (65 años en adelante).
 """
-
+"""
 categoria_edad = int(input("Ingresa edad: "))
 
-if categoria_edad <= 0 or categoria_edad <= 12:
+if categoria_edad  <= 0 or categoria_edad <= 12:
     print(f"Tu edad es {categoria_edad} eres un niño.")
 
 elif categoria_edad <= 13 or categoria_edad <= 19:
     print (f"Tu edad es {categoria_edad} eres un adolescente.")
 
 elif categoria_edad <= 20 or categoria_edad <= 64:
+    print(f"Tu edad es {categoria_edad} eres un adulto.")
+
+else:
+    print(f"Tu edad es {categoria_edad} eres un adulto mayor.")
+"""
+categoria_edad = int(input("Ingresa edad: "))
+
+if 0 <= categoria_edad <= 12:
+    print(f"Tu edad es {categoria_edad} eres un niño.")
+
+elif 13 <= categoria_edad <= 19:
+    print (f"Tu edad es {categoria_edad} eres un adolescente.")
+
+elif 20 <= categoria_edad <= 64:
     print(f"Tu edad es {categoria_edad} eres un adulto.")
 
 else:
