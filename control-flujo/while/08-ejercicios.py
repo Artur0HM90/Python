e54218c99f52dd33f4a3a4823e01c1aadac1946c
Artur0HM90 <arturohurtadomendez90@gmail.com>
2023-12-30 22:27:01 -0500

    Ejercicio 8 mod while

--- control-flujo/while/08-ejercicios.py
+++ control-flujo/while/08-ejercicios.py
@@ -1,3 +1,6 @@
 """
 "¿Cómo implementarías en un programa la condición para aplicar un descuento del 10% si el cliente ha comprado más de 5 productos? Utiliza sola la estructura de control if.
 """
+
+print("Bienvenidos a la tienda de los descuentos.")
+print("Esta semana hay un descuento del 10% por la compra de mas de 4 produtos.")
