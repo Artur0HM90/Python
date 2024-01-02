a9b54565aaa33bf3eea6e12003391fb0ae792158
Artur0HM90 <arturohurtadomendez90@gmail.com>
2024-01-02 12:02:40 -0500

    Ejercicio final con while - if - for

--- control-flujo/07-while.py
+++ control-flujo/07-while.py
@@ -11,7 +11,15 @@
 #    print(comando)
 
 while True:
-    comando = input("$ ")
+    print("Ingresa un número entero positivo.")
+
+    ingresa_numero = int(input("Ingresa un número: "))
+
+    if ingresa_numero > 0:
+        for par in range (1, ingresa_numero):
+            if par % 2 == 0:
+                print(f"ingresaste iun par {par}")
+    comando = input("Si deseas salir escribe - 'salir' si desea continuar presiona enter: ")
     print(comando)
     if comando.lower() == "salir":
         break
--- control-flujo/while/08-ejercicios.py
+++ control-flujo/while/08-ejercicios.py
@@ -1,6 +1,29 @@
 """
-"¿Cómo implementarías en un programa la condición para aplicar un descuento del 10% si el cliente ha comprado más de 5 productos? Utiliza sola la estructura de control if.
+Escribe un programa que solicite al usuario ingresar un número entero positivo. Luego, el programa debe contar y mostrar la cantidad de números pares e impares desde 1 hasta el número ingresado, utilizando un bucle for y un bucle while. Asegúrate de incluir instrucciones if para determinar si un número es par o impar.
 """
 
-print("Bienvenidos a la tienda de los descuentos.")
-print("Esta semana hay un descuento del 10% por la compra de mas de 4 produtos.")
+print("Determinar si un número es par o impar.")
+
+while True:
+    numero = int(input("Ingresa un número: "))
+    print(f"""ingresaste el {numero} quieres saber los números pares o los impares 
+    1. Paras 
+    2. Impares""")
+    par_impar = int(input("Que vas elegir: "))
+
+    if par_impar == 1:
+        print(f"Elegiste averiguar los números pares del {numero}.")
+        for par in range (numero):
+            if par % 2 == 0:
+                print (par) 
+
+    elif par_impar == 2:
+        print(f"Elegiste averiguar los números impares del {numero}.")
+        for impar in range (numero):
+            if impar % 2 == 0:
+                print (impar + 1)
+
+    salir = (str(input("Si desea salir escriba - 'salir' - si desea continuar presione enter: "))).lower()
+    print (salir)
+    if salir == "salir":
+        break
