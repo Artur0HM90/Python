06f34377fb6ff89c5c0f3c11070bc43362055875
Artur0HM90 <arturohurtadomendez90@gmail.com>
2024-01-05 23:20:27 -0500

    funcion

---
+++ funciones/ejerciosios/03-ejercicios.py
@@ -0,0 +1,4 @@
+"""
+Calculadora básica:
+Crea una calculadora simple usando funciones. La calculadora debe ser capaz de realizar operaciones de suma, resta, multiplicación y división. Define funciones separadas para cada operación.
+"""
