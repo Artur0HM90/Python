c7a1812ccd2516e45490658e246e5dd6e6afb366
Artur0HM90 <arturohurtadomendez90@gmail.com>
2024-01-06 20:03:22 -0500

    ejercicio de funciones 0 3

--- funciones/ejerciosios/03-ejercicios.py
+++ funciones/ejerciosios/03-ejercicios.py
@@ -2,50 +2,59 @@
 Calculadora básica:
 Crea una calculadora simple usando funciones. La calculadora debe ser capaz de realizar operaciones de suma, resta, multiplicación y división. Define funciones separadas para cada operación.
 """
-while True:
-    def suma (primer_numero, segundo_numero):
-        return primer_numero + segundo_numero
 
-    def resta (primer_numero, segundo_numero):
+while True:
+    def suma(primer_numero, segundo_numero):
+        return primer_numero + segundo_numero   # me falto el return
+
+    def resta(primer_numero, segundo_numero):
         return primer_numero - segundo_numero
 
-    def multiplicación (primer_numero, segundo_numero):
+    def multiplicacion(primer_numero, segundo_numero):
         return primer_numero * segundo_numero
 
-    def division (primer_numero, segundo_numero):
+    def division(primer_numero, segundo_numero):
         if segundo_numero != 0:
             return primer_numero / segundo_numero
         else:
-            return "ERROR - División por cero."
-        
-    def desarrollo ():
-        print ("Calculadora basica - que vas elige.")
+            return ("No puedes dividir entre cero.")
+
+
+
+    def calculadora():
+        print("Calculadora basica.")
         print("1: SUMA")
         print("2: RESTA")
         print("3: MULTIPLICACIÓN")
         print("4: DIVISIÓN")
+        elegir = int(input("Que vas elegir - 1/2/3/4: "))
 
-        opcion = int(input("Cual vas elegir 1/2/3/4: " ))
+        primer_numero = float(input("Ingresa el primer número: "))
+        segundo_numero = float(input("Ingresa el segundo número: "))
 
-        primer_numero = float(input("Ingresa primer número: "))
-        segundo_numero = float(input("Ingresa segundo número: "))
-
-        if opcion == 1:
+        if elegir == 1:
             print("Elegiste la SUMA.")
-            resultado = suma(primer_numero,segundo_numero)
-
-        elif opcion == 2:
-            print("Elegisate la RESTA.")
-            resultado = resta(primer_numero,segundo_numero)
+            resultado = suma(primer_numero, segundo_numero)
+            print(f"El resultado de {primer_numero} + {segundo_numero} = {resultado}")
         
-        elif opcion == 3:
-            print("Elegiste la MULTIPLICAIÓN.")
-            resultado = multiplicación(primer_numero,segundo_numero)
+        elif elegir == 2:
+            print("Elegiste la RESTA.")
+            resultado = resta(primer_numero, segundo_numero)
+            print(f"El resultado de {primer_numero} - {segundo_numero} = {resultado}")
 
-        elif opcion == 4:
-            print("Elegiaste la DIVISIÓN.")
-            resultado = division(primer_numero,segundo_numero)
+        elif elegir == 3:
+            print("Elegiste la MULTIPLICACIÓN.")
+            resultado = multiplicacion(primer_numero, segundo_numero)
+            print(f"El resultado de {primer_numero} x {segundo_numero} = {resultado}")
         
-        print(f"---> El resultado es: {resultado}")
-    desarrollo()
-    print("-------------------------")
+        elif elegir == 4:
+            print("Elegiste la DIVISIÓN.")
+            resultado = division(primer_numero, segundo_numero)
+            print(f"El resultado es {primer_numero} / {segundo_numero} = {resultado}")
+
+
+    calculadora()
+    print("--------------------------")
+
+
+
