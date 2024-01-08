002027a97388290be45f125509aaeec76af3887c
Artur0HM90 <arturohurtadomendez90@gmail.com>
2024-01-08 17:10:35 -0500

    depuracion

---
+++ funciones/05-alcance.py
@@ -0,0 +1,12 @@
+saludo = "Hola global"
+
+def saludar():
+    saludo = "Hola Mundo"
+    print(saludo)
+
+def saludarChanchito():
+    saludo = "Hola Chanchito"
+    print(saludo)
+
+saludar()
+print(saludo)
