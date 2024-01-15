8232591e0177de61f6c2f66ce2654862b3f50a25
Artur0HM90 <arturohurtadomendez90@gmail.com>
2024-01-15 10:10:34 -0500

    manipulacion de listas

--- tipos-avanzados/02-manipulando-listas.py
+++ tipos-avanzados/02-manipulando-listas.py
@@ -1,9 +1,10 @@
 mascotas = ["Wolfgang", "Pelusa", "Pulga", "Copito"]
 print(mascotas[0])
-mascotas[0] = "Bicho" #    cambiar elementos
+mascotas[0] = "Bicho" #    cambiar el primer elementos
 #print(mascotas)
 #print(mascotas[-1])
 #print(mascotas[1::2])
 
 numeros = list(range(21))
-print(numeros)
+print(numeros[::2]) # con esto nos da los numeros pares
+print(numeros[1::2]) # con esto nos da los numeros inpares
