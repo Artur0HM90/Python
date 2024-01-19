605ed7fd32656604e780035406b43656d468c488
Artur0HM90 <arturohurtadomendez90@gmail.com>
2024-01-18 23:01:38 -0500

    ejercicio 9

--- ejercicios/09-ejercicios.py
+++ ejercicios/09-ejercicios.py
@@ -1,3 +1,9 @@
 """
 Nested Loops: Crea un programa que utilice bucles anidados para imprimir un patrón de asteriscos en forma de cuadrado.
 """
+numero = int(input("Ingresa un número: "))
+
+for inicio in range(numero):
+    for final in range(numero):
+        print("* ", end=" ")
+    print()
