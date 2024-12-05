378dc07dee17425c04f1a89cb20e7de2e5c07b67
Hurtado Méndez Arturo <arturohurtadomendez90@gmail.com>
2024-12-05 18:05:11 -0500

    clase 6

---
+++ 05-clase_6/clase6.py
@@ -0,0 +1,39 @@
+# operadores numericos
+
+a = 10
+b = 3
+
+print("Suma:", a + b)
+print("Resta:", a - b)
+print("Multiplicación:", a * b)
+print("División:", a / b)
+
+# modulo (%)
+print("Modulo:", a % b)
+
+# parte entera de la division (//)
+print("Parte entera división:", a // b)
+
+# potencioación (**)
+print("Potenciación:", a ** b)
+
+# hay operaciones donde se puede escribir de una forma mas concisa se le llama SHORTCUTS
+# usaremos la variable a y le sumaremos 2
+
+# esto es una forma 
+a = a + 2
+print(a)
+
+# esto es otra forma 
+a +=2
+print(a)
+
+operation_1 = 2 + 3 * 4
+operation_2 = 2 + (3 * 4)
+operation_3 = (2 + 3) * 4
+print(operation_1)
+print(operation_2)
+print(operation_3)
+
+operation_4 = (2+3) * (4**2) / 8 - 1
+print(operation_4)
