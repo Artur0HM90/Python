0b7032dd0ff9a3a66661eabe8c843d72d782720a
Hurtado Méndez Arturo <arturohurtadomendez90@gmail.com>
2024-12-05 17:34:28 -0500

    Clase 5

---
+++ 05-clase_5/clase5.py
@@ -0,0 +1,24 @@
+x = 10
+y = 5.678
+z = 1.2e6
+a = 1.2e-6
+print(type(x)) # este es un número punto entero
+print(type(y)) # este es un número punto flotante
+
+#En python caundo cuando queremos usar numeros muy grandes o muy pequeños se recomienda usa la notacion cientifica
+#print(z)
+#print(a)
+
+print (x + x)
+print (x + y)
+print (y + y)
+
+
+# clase boolean que solo puede representar dos estados verdadero o falso
+
+is_tru = True
+is_false = False
+
+print(is_tru)
+print(type(is_false))
+print("Nunca" "pares" "de" "aprender", sep=", ")
