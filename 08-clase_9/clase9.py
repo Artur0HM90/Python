7fc7e3ecfe2b21af974459ef7bc8f704b34bd07f
Hurtado Méndez Arturo <arturohurtadomendez90@gmail.com>
2024-12-08 17:00:39 -0500

    clase 9 - matrices - tuplas

---
+++ 08-clase_9/clase9.py
@@ -0,0 +1,16 @@
+#dimensiones
+matrix = [[1,2,3],
+        [4,5,6],
+        [7,8,9]]
+
+print(matrix)
+print(matrix[0])
+print(type(matrix))
+
+#tuplas en las tuplas no podemos realizar ninguna modificación ß
+
+numbers = (1,2,3,4,5)
+print(numbers)
+print(type(numbers))
+print(numbers[0])
+# numbers [0] = 'unos' # en las tuplas no podemos realizar ninguna modificación 
