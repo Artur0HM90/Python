87eb510af42ca921974485d0134deca01e2b3fb0
Hurtado Méndez Arturo <arturohurtadomendez90@gmail.com>
2024-12-22 12:43:43 -0500

    12 clase uso del if

---
+++ 11-clase-12/clase12-2.py
@@ -0,0 +1,11 @@
+x = 15
+y = 20
+
+if x > 10 and y > 25:
+    print("X es mayor quew 10 y Y es mayor que 15.")
+
+if x > 10 or y > 25:
+    print("X es mayor quew 10 o Y es mayor que 15.")
+
+if not x > 10: # con not negamos que esto sea verdadero.
+    print("X no es mayot que 10.")
---
+++ 11-clase-12/clase12-3.py
@@ -0,0 +1,11 @@
+is_membre = True
+age = 15
+
+if is_membre:
+    if age  >= 15:
+        print("Tienes acceso ya que eres miembro y mayor o igual a 15 años.")
+    else:
+        print("No tienes acceso ya eres miembro pero meor a 15 años.")
+else:
+    print("No eres miembro y NO TIENES ACCESO.")
+    
