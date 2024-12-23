940c4a7f1ab70fc6a26d714e3b7f34068478d31e
Hurtado Méndez Arturo <arturohurtadomendez90@gmail.com>
2024-12-22 22:06:12 -0500

    14. uso iter.

---
+++ 13-clase-14/clase14-3.py
@@ -0,0 +1,12 @@
+# Crear un iterador para los numeros impares
+
+# Limite 
+
+limit = 10
+
+# Crear iterador 
+odd_itter = iter(range(1, limit + 1, 2))
+
+# usar iterador 
+for num in odd_itter:
+    print(num)
